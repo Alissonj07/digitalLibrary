e792d88195b29a54d27e2869adda5064377c3076
Alisson J. dos Santos Silva <142062431+Alissonj07@users.noreply.github.com>
2025-05-11 23:20:02 -0300

    Atualizar o UsuarioRepository.cs

---
+++ BibliotecaDigital/Data/UsuarioRepository.cs
@@ -0,0 +1,40 @@
+using BibliotecaDigital.Models;
+
+namespace BibliotecaDigital.Data;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDataContext _context;
+
+    public UsuarioRepository(AppDataContext context)
+    {
+        _context = context;
+    }
+
+    public Usuario BuscarPorEmail(string email)
+    {
+        return _context.Usuarios.FirstOrDefault(u => u.Email == email);
+    }
+
+    public Usuario BuscarPorId(int id)
+    {
+        return _context.Usuarios.FirstOrDefault(u => u.Id == id);
+    }
+
+    public void Cadastrar(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        _context.SaveChanges();
+    }
+
+    public List<Usuario> Listar()
+    {
+        return _context.Usuarios.ToList();
+    }
+
+    public void Deletar(Usuario usuario)
+    {
+        _context.Usuarios.Remove(usuario);
+        _context.SaveChanges();
+    }
+}
