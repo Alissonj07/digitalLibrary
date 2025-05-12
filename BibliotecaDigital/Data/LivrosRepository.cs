882fa59b3f66785561a9af572cec3edf7cd130e4
Alisson J. dos Santos Silva <142062431+Alissonj07@users.noreply.github.com>
2025-05-11 23:19:40 -0300

    Atualizar o LivrosRepository.cs

---
+++ BibliotecaDigital/Data/LivrosRepository.cs
@@ -0,0 +1,45 @@
+using BibliotecaDigital.Models;
+
+namespace BibliotecaDigital.Data;
+
+public class LivrosRepository : ILivrosRepository
+{
+    private readonly AppDataContext _context;
+
+    public LivrosRepository(AppDataContext context)
+    {
+        _context = context;
+    }
+
+    public List<Livros> Listar()
+    {
+        return _context.Livros.ToList();
+    }
+
+    public List<Livros> ListarPorAutor(int autorId)
+    {
+        return _context.Livros.Where(l => l.AutorId == autorId).ToList();
+    }
+
+    public Livros BuscarPorId(int id)
+    {
+        return _context.Livros.FirstOrDefault(l => l.Id == id);
+    }
+
+    public void Cadastrar(Livros livro)
+    {
+        _context.Livros.Add(livro);
+        _context.SaveChanges();
+    }
+
+    public void Deletar(Livros livro)
+    {
+        _context.Livros.Remove(livro);
+        _context.SaveChanges();
+    }
+
+    public List<Livros> ListarDisponiveis()
+    {
+        return _context.Livros.Where(l => !l.Emprestado).ToList();
+    }
+}
